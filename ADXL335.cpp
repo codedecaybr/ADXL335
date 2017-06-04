94aabca48d7a879e0a674b06b24698e0c1f24251
Renan Yuri Lino <renan.lino@gmail.com>
2017-06-03 22:53:00 -0300

    First commit

---
+++ ADXL335.cpp
@@ -0,0 +1,94 @@
+/*
+Copyright 2017 Code Decay
+Authors: Renan Yuri Lino, Douglas Navarro
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include "ADXL335.h"
+
+ADXL335::ADXL335(int x, int y, int z) {
+  pinX = x; pinY = y; pinZ = z;
+  xMin = 512; xMax = 512; yMin = 512; yMax = 512; zMin = 512; zMax = 512;
+  samples = 10;
+}
+
+ADXL335::~ADXL335(){
+
+}
+
+int ADXL335::averageRead(int pin) {
+  long value = 0;
+  for (int i = 0; i < samples; i++)
+  {
+    value += analogRead(pin);
+  }
+  return value/samples;
+}
+
+int ADXL335::getRawX() {
+  return averageRead(pinX);
+}
+
+int ADXL335::getRawY() {
+  return averageRead(pinY);
+}
+
+int ADXL335::getRawZ() {
+  return averageRead(pinZ);
+}
+
+double ADXL335::getGX() {
+  int x = getRawX();
+  return map(x, xMin, xMax, -1000, 1000)/1000.0;
+}
+
+double ADXL335::getGY() {
+  int y = getRawY();
+  return map(y, xMin, xMax, -1000, 1000)/1000.0;
+}
+
+double ADXL335::getGZ() {
+  int z = getRawX();
+  return map(z, xMin, xMax, -1000, 1000)/1000.0;
+}
+
+void ADXL335::calibrate() {
+  int x = getRawX();
+  int y = getRawY();
+  int z = getRawZ();
+  if (x < xMin) xMin = x;
+  if (x > xMax) xMax = x;
+
+  if (y < yMin) yMin = y;
+  if (y > yMax) yMax = y;
+
+  if (z < zMin) zMin = z;
+  if (z > zMax) zMax = z;
+}
+
+void ADXL335::preset() {
+  xMin = 394;
+  xMax = 593;
+
+  yMin = 393;
+  yMax = 591;
+
+  zMin = 401;
+  zMax = 601;
+}
---
+++ README.md
@@ -0,0 +1,59 @@
+# Arduino Library for ADXL335
+
+## ADXL335( int x, int y, int z )
+
+Constructor. x, y and z parameters are analog pins connected to ADXL analog output.
+
+# preset()
+
+Calibrate ADXL335 with default values.
+
+# calibrate()
+
+Manual calibration. Must be done with all 6 valid positions (y, -y, x, -x, z, -z).
+
+## getRawX(), getRawY(), getRawZ()
+
+Read axis acceleration from ADXL335 as raw analogRead() data (with average).
+
+## getGX(), getGY(), getGZ()
+
+Read axis acceleration from ADXL335 as G units (9.8m/s² = 1G)
+
+# Example
+
+```Arduino
+
+#include "ADXL335.h"
+
+// CONSTRUCTOR
+ADXL335 acelerometro(A0, A1, A2);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  analogReference(EXTERNAL);
+  acelerometro.preset();
+
+}
+
+void loop() {
+
+  float xG = acelerometro.getGX();
+  float yG = acelerometro.getGY();
+  float zG = acelerometro.getGZ();
+
+  Serial.print(xG);
+  Serial.print("G, ");
+  Serial.print(yG);
+  Serial.print("G, ");
+  Serial.print(zG);
+  Serial.println("G");
+  Serial.print("Inclinacao em graus: ");
+  Serial.println(anguloG);
+  Serial.println();
+
+  delay(500);
+}
+
+```
